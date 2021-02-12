4e0c7005fc1b55471865071cf0bf6f6eecf38c28
pandoragear <andy.dang.it@gmail.com>
2021-02-11 22:17:30 -0600

    Add Helpers to JS

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ function handleSubmitForm(e){
         addToDoList(input.value);
         input.value = ";"
 }
+
+// Helpers
+function addToDoList(todo) {
+    let ul= document.querySelector('ul');
+    let li = document.createElement('li');
+
+    li.innerHTML = `
+        <span class="todo-item">${todo}</span>
+        <button name="checkButton"><i class="fas fa-check-square" </i> </button> 
+        <button name="deleteButton"><i class="fas fa-trash"> </i> </button
+    `;
+}
